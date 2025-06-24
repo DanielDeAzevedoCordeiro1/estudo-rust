65f58ea021e9292853149f454e80786d0ecc0104
daniel <daniel.cordeiro2@fatec.sp.gov.br>
2025-06-23 21:58:32 -0300

    Estruturas condicionais if/else if/else em Rust

---
+++ src/estruturas_condicionais_rust/02.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let numero = 7;
+    
+    if numero < 5 {
+        println!("O número é menor que 5");
+    } else if numero < 10 {
+        println!("O número está entre 5 e 9");
+    } else {
+        println!("O número é 10 ou maior");
+    }
+}
